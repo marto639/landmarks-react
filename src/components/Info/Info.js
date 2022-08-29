0fda0f7e01ac3ed31b8ada1bec2c8c9cc024207c
marto639 <martin639@abv.bg>
2022-08-29 10:18:30 +0300

    Added information in info page
    Changed background color for the whole page, navigation and edit page

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,24 +1,30 @@
 export const Info = () => {
     return (
         <h3 className="info-text">
-            Images may be two or three-dimensional, such as a photograph or screen
-            display, or three-dimensional, such as a statue or hologram. They may be
-            captured by optical devices - such as cameras, mirrors, lenses, telescopes,
-            microscopes, etc. and natural objects and phenomena, such as the human eye or
-            water. The word 'image' is also used in the broader sense of any
-            two-dimensional figure such as a map, a graph, a pie chart, a painting or a
-            banner. In this wider sense, images can also be rendered manually, such as by
-            drawing, the art of painting, carving, rendered automatically by printing or
-            computer graphics technology, or developed by a combination of methods,
-            especially in a pseudo-photograph. A volatile image is one that exists only
-            for a short period of time. This may be a reflection of an object by a mirror,
-            a projection of a camera obscura, or a scene displayed on a cathode ray tube.
-            A fixed image, also called a hard copy, is one that has been recorded on a
-            material object, such as paper or textile by photography or any other digital
-            process. A mental image exists in an individual's mind, as something one
-            remembers or imagines. The subject of an image need not be real; it may be an
-            abstract concept, such as a graph, function, or imaginary entity. For example,
-            Sigmund Freud claimed to have dreamed purely in aural-images of dialogs.
+            <div>
+                <li>
+                    This Website was created for lovers who visit beautiful cities.
+                </li>
+            </div>
+            <div>
+                <li>
+                    For those who like to share the sights they have admired can join our site
+                    and post beautiful photos for people who don't have the opportunity to see them live.
+                </li>
+            </div>
+            <div>
+                <li>
+                    Each object is distinguished by its characteristics.
+                </li>
+            </div>
+            <div>
+                <li>
+                    If you have a problem with logging in, registering or creating a post, contact us by email: marto89563@gmail.com
+                    <div>
+                        and we will contact you as soon as possible.
+                    </div>
+                </li>
+            </div>
         </h3>
     );
 };
